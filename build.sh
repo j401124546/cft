2101ec914e91f6db0e1c2f6c083a812e981ea51b
shiguojq <401124546@qq.com>
2022-04-17 21:25:21 +0800

    update author

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ mkdir "${DIR}/output/config"
 go build -o "${DIR}/output/app"
 cp "${DIR}/config/application.json" "${DIR}/output/config"
 
-git log --author="jiayuke" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines : %s, total lines: %s\n", add, subs, loc }'
+git log --author="shiguojq" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines : %s, total lines: %s\n", add, subs, loc }'
