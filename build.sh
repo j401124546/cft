251cb69965113999e44fa68c1210a749a8008cf7
shiguojq <401124546@qq.com>
2022-04-17 21:24:42 +0800

    add code statics

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 DIR=$(pwd)
-echo $DIR
 
 if [ -d "${DIR}/output" ]
 then
@@ -12,3 +11,5 @@ mkdir "${DIR}/output/config"
 
 go build -o "${DIR}/output/app"
 cp "${DIR}/config/application.json" "${DIR}/output/config"
+
+git log --author="jiayuke" --pretty=tformat: --numstat | awk '{ add += $1; subs += $2; loc += $1 - $2 } END { printf "added lines: %s, removed lines : %s, total lines: %s\n", add, subs, loc }'
